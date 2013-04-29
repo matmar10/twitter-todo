412dec2d4cb1ef99b76d71aba1368f85bb7c57c9
Matthew J. Martin <matthew.mar10@gmail.com>
2013-04-28 22:17:23 -0700

    add basic task app

--- src/Matmar10/Bundle/TodoBundle/Resources/public/src/js/app.js
+++ src/Matmar10/Bundle/TodoBundle/Resources/public/src/js/app.js
@@ -1,31 +1,54 @@
-(function($) {
-
-        // options
-    var o = {};
 
 
-    $.todo = {};
-    $.todo.defaults = {
-        elements: {
-            messagesContainer: "ul.messages",
-            usersContainer: "ul.users"
+function TodoController($scope) {
+
+    // initialize controller's model
+    $scope.todos = [
+        {
+            text: "learn Angular.js",
+            done: false
+        },
+        {
+            text: "build Angular app",
+            done: false
+        },
+        {
+            text: "show off Angular app",
+            done: false
         }
+    ];
+
+    // Define member functions
+    $scope.addTodo = function() {
+        window.console.log(this);
+        $scope.todos.push({
+            text: this.todoText,
+            done: false
+        });
+        $scope.remaining++;
+        $scope.todoText = "";
     };
 
-    /**
-     * Initializes the spreecast chat module
-     *
-     * @param options Optional parameters to override default options
-     */
-    $.todo.init = function(options) {
-
-        // set up page options
-        o = $.extend({}, $.todo.defaults, options);
-
-        // cache commonly used elements
-        $.each(o.elements, function(cacheId, selector) {
-            $elementCache[cacheId] = $(selector);
+    $scope.recalc = function() {
+        $scope.remaining = $scope.todos.length;
+        angular.forEach($scope.todos, function(todo) {
+            if(todo.done) {
+                $scope.remaining--;
+            }
         });
     };
 
-})(jQuery);
+    $scope.removeDone = function() {
+        var todos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(todos, function(todo) {
+            if (!todo.done) {
+                $scope.todos.push(todo);
+            }
+        });
+    };
+
+    // call recalc once to update the model's 'remaining' value
+    // Note: this needs to be called after definition
+    $scope.recalc();
+}
